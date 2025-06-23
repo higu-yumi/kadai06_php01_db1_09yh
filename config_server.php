7cace4da4937392c783fc0b69fc39b2c7f20be51
higu-yumi <207814247+higu-yumi@users.noreply.github.com>
2025-06-23 17:51:22 +0900

    Delete config_server.php

--- config_server.php
+++
@@ -1,9 +0,0 @@
-<?php
-// config_server.php (サーバー用)
-
-// さくらサーバーのコントロールパネルで確認した情報に合わせてください
-define('DB_HOST', '******');
-define('DB_NAME', '******'); 
-define('DB_USER', '******'); 
-define('DB_PASS', ''); 
-?>
