5824d20aee8eff5089ee4dd111ad214227c2d881
higu-yumi <207814247+higu-yumi@users.noreply.github.com>
2025-06-23 10:31:37 +0900

    Update config_server.php

--- config_server.php
+++ config_server.php
@@ -2,8 +2,8 @@
 // config_server.php (サーバー用)
 
 // さくらサーバーのコントロールパネルで確認した情報に合わせてください
-define('DB_HOST', 'mysql3108.db.sakura.ne.jp'); // 例: 'mysql0000.db.sakura.ne.jp'
-define('DB_NAME', 'yh-deploy_gs_kadai_user'); // 例: 'dy_gs_db'
-define('DB_USER', 'yh-deploy_gs_kadai_user');     // 例: 'dy_gs_user'
-define('DB_PASS', '55dekitaphp2525');   // 例: '5p2525'
+define('DB_HOST', '*****');
+define('DB_NAME', '*****');
+define('DB_USER', '*****');
+define('DB_PASS', '');
 ?>
