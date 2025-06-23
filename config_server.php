3a5d663434b263f23f3081a6bc2595743d6bc63b
higu-yumi <207814247+higu-yumi@users.noreply.github.com>
2025-06-23 11:20:29 +0900

    Add files via upload

---
+++ config_server.php
@@ -0,0 +1,9 @@
+<?php
+// config_server.php (サーバー用)
+
+// さくらサーバーのコントロールパネルで確認した情報に合わせてください
+define('DB_HOST', '******');
+define('DB_NAME', '******'); 
+define('DB_USER', '******'); 
+define('DB_PASS', ''); 
+?>
